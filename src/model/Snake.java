566b19e0d5d285fdef3cfdb17cd6af934a4950de
munlikaCh <munlika_ch@cmu.ac.th>
2021-09-22 14:23:26 +0700

    5.5.1 The snake’s head, body, and tail

--- src/model/Snake.java
+++ src/model/Snake.java
@@ -1,4 +1,37 @@
 package model;
 
+import javafx.geometry.Point2D;
+
+import java.util.ArrayList;
+
 public class Snake {
+    private Direction direction;
+    private Point2D head;
+    private Point2D prev_tail;
+    private ArrayList<Point2D> body;
+
+    public Snake(Point2D position){
+        direction = Direction.DOWN;
+        body = new ArrayList<>();
+        this.head = position;
+        this.body.add(this.head);
+    }
+
+    public void update() {
+        head = head.add(direction.current);
+        prev_tail = body.remove(body.size() - 1);
+        body.add(0, head);
+    }
+
+    public void setCurrentDirection(Direction direction) {
+        this.direction = direction;
+    }
+
+    public Direction getCurrentDirection() {
+        return this.direction;
+    }
+
+    public Point2D getHead() {
+        return head;
+    }
 }
--- src/test/SnakeTest.java
+++ src/test/SnakeTest.java
@@ -1,4 +1,36 @@
 package test;
 
+import de.saxsys.mvvmfx.testingutils.jfxrunner.JfxRunner;
+import javafx.geometry.Point2D;
+import model.Direction;
+import model.Food;
+import model.Snake;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static junit.framework.TestCase.*;
+
+//Imports are omitted
+@RunWith(JfxRunner.class)
+
 public class SnakeTest {
+    private Snake snake;
+
+    @Before
+    public void setup() {
+        snake = new Snake(new Point2D(0, 0));
+    }
+
+    @Test
+    public void snakeShouldBeSpawnAtTheCoordinateItWasCreated() {
+        assertEquals(snake.getHead(), new Point2D(0, 0));
+    }
+
+    @Test
+    public void snakeShouldMoveDownwardIfItIsHeadingDownward() {
+        snake.setCurrentDirection(Direction.DOWN);
+        snake.update();
+        assertEquals(snake.getHead(), new Point2D(0, 1));
+    }
+
 }
