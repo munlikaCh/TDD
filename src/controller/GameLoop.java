389b7b34b3d2924801b42107ee04657eb4169f8e
munlikaCh <munlika_ch@cmu.ac.th>
2021-09-22 15:21:22 +0700

    5.5.4 The game loop

--- src/controller/GameLoop.java
+++ src/controller/GameLoop.java
@@ -1,4 +1,79 @@
 package controller;
 
-public class GameLoop {
+import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
+import javafx.stage.Popup;
+import model.Direction;
+import model.Food;
+import model.Snake;
+import view.Platform;
+
+import java.util.ArrayList;
+
+public class GameLoop implements Runnable {
+    private Platform platform;
+    private Snake snake;
+    private Food food;
+    private float interval = 1000.0f / 10;
+    private boolean running;
+
+    public GameLoop(Platform platform, Snake snake, Food food) {
+        this.snake = snake;
+        this.platform = platform;
+        this.food = food;
+        running = true;
+    }
+
+    private void update() {
+        KeyCode cur_key = platform.getKey();
+        Direction cur_direction = snake.getCurrentDirection();
+
+        if (cur_key == KeyCode.UP && cur_direction != Direction.DOWN)
+            snake.setCurrentDirection(Direction.UP);
+        else if (cur_key == KeyCode.DOWN && cur_direction != Direction.UP)
+            snake.setCurrentDirection(Direction.DOWN);
+        else if (cur_key == KeyCode.LEFT && cur_direction != Direction.RIGHT)
+            snake.setCurrentDirection(Direction.LEFT);
+        else if (cur_key == KeyCode.RIGHT && cur_direction != Direction.LEFT)
+            snake.setCurrentDirection(Direction.RIGHT);
+
+        snake.update();
+    }
+
+    private void checkCollision() {
+        if (snake.isCollidingWith(food)) {
+            snake.grow();
+            food.respawn();
+        }
+        if (snake.isDead()) {
+            running = false;
+        }
+    }
+
+    private void redraw() {
+        platform.render(snake,food);
+    }
+
+    @Override
+    public void run() {
+        while (running) {
+            update();
+            checkCollision();
+            redraw();
+            try {
+                Thread.sleep((long) interval);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    //add
+    public Platform getPlatform() {
+        return platform;
+    }
+
+    public Snake getSnake() {
+        return snake;
+    }
 }
--- src/controller/Launcher.java
+++ src/controller/Launcher.java
@@ -1,4 +1,30 @@
 package controller;
 
-public class Launcher {
+import javafx.application.Application;
+import javafx.geometry.Point2D;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import model.Food;
+import model.Snake;
+import view.Platform;
+
+public class Launcher extends Application {
+    public static void main(String[] args) { launch(args); }
+
+    @Override
+    public void start(Stage primaryStage){
+        Platform platform = new Platform();
+        Snake snake = new Snake(new Point2D(platform.WIDTH/2,platform.HEIGHT/2));
+        Food food = new Food();
+        GameLoop gameLoop = new GameLoop(platform,snake,food);
+        Scene scene = new Scene(platform,platform.WIDTH*platform.TILE_SIZE,platform
+                .HEIGHT * platform.TILE_SIZE);
+        scene.setOnKeyPressed(event-> platform.setKey(event.getCode()));
+        scene.setOnKeyReleased(event -> platform.setKey(null));
+        primaryStage.setTitle("Snake");
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.show();
+        (new Thread(gameLoop)).start();
+    }
 }
